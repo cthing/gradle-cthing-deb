d3003c4203dc6a0fe711575c0ed9d635cded9520
Baron Roberts <baron@cthing.com>
2021-12-23 13:35:11 -0800

    Upgrade Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
     implementation("org.freemarker:freemarker:2.3.31")
     implementation("org.cthing:projectinfo:1.0.0-+")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
