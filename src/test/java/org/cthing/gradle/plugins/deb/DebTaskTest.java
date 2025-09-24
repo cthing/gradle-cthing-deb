acc4e09d1e8880c96492c7d284b8123f2888ecd1
Baron Roberts <baron@cthing.com>
2025-09-24 14:00:06 -0700

    Fixes for failing GH Actions build.

--- src/test/java/org/cthing/gradle/plugins/deb/DebTaskTest.java
+++ src/test/java/org/cthing/gradle/plugins/deb/DebTaskTest.java
@@ -7,6 +7,7 @@ package org.cthing.gradle.plugins.deb;
 import java.util.Date;
 import java.util.concurrent.Callable;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 import org.cthing.projectversion.BuildType;
 import org.cthing.projectversion.ProjectVersion;
@@ -17,12 +18,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DebTaskTest {
 
+    private static final Pattern CHANGLOG_DATE_PATTERN =
+            Pattern.compile("(Mon|Tue|Wed|Thu|Fri|Sat|Sun), \\d{1,2} "
+                                    + "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) "
+                                    + "\\d{4} \\d{2}:\\d{2}:\\d{2} [\\-+]\\d{4}");
+
     @Test
     public void testGetChangelogDate() {
         final Date date = new Date(1638042324837L);
         final ProjectVersion version = new ProjectVersion("1.2.3", BuildType.release, date);
         assertThat(version.getBuildDate()).isEqualTo("2021-11-27T19:45:24Z");
-        assertThat(DebTask.getChangelogDate(version)).isEqualTo("Sat, 27 Nov 2021 11:45:24 -0800");
+        assertThat(DebTask.getChangelogDate(version)).matches(CHANGLOG_DATE_PATTERN);
     }
 
     @Test
